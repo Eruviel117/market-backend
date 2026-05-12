036a019cdb8a99ab225d912b91c1db0203067dce
Euruviel <euruviel.marquez@tecdesoftware.edu.mx>
2026-05-12 08:18:23 -0600

    test: adding HelloWorldController to test Spring

---
+++ src/main/java/mx/edu/tecdesoftware/market_backend/HelloWorldController.java
@@ -0,0 +1,13 @@
+package mx.edu.tecdesoftware.market_backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HelloWorldController {
+    @GetMapping("/hola")
+    public String saludar(){
+        return "Hello World";
+    }
+}
